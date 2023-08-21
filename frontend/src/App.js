033be4adf7571250783dc2ccda3157cb6f3c986c
Ahmet Cigil <ahmetcgln@gmail.com>
2023-08-22 00:50:09 +0200

    frondend file structure and installations done

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/icons-material";
+import "./App.css";
+
+function App() {
+	return (
+		<Box className="App">
+			<Typography>Hi</Typography>
+		</Box>
+	);
+}
+
+export default App;
