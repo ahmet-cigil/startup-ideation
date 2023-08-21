81cb209bb67b03dcfad4cc0961adab6e598dae27
Ahmet Cigil <ahmetcgln@gmail.com>
2023-08-21 16:06:31 +0200

    useRoutes, userController and error-exception handling done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log("Hi");
+// Configuration
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
+
+// Initialize
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => {
+	res.status(200).json({ msg: "Welcome to the support desk API" });
+});
+
+// Routes from 'userRoutes' file
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+// Listen
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
